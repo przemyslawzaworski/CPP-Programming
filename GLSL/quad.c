9f2a76ad37904eb5e79613e48099937269f307d8
Przemyslaw Zaworski <przemyslawzaworski@gmail.com>
2019-06-29 17:16:41 +0200

    cleanup

--- GLSL/quad.c
+++ GLSL/quad.c
@@ -38,7 +38,7 @@ static const char* VertexShader = \
 	"void main()"
 	"{"	
 		"gl_Position = vec4(vertex, 2.0);"
-		"UV = uv;"		
+		"UV = uv;"
 	"}";
 	
 static const char* FragmentShader = \
@@ -108,8 +108,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	HWND hwnd = CreateWindowEx(0, win.lpszClassName, "Demo", WS_VISIBLE|WS_OVERLAPPEDWINDOW, 0, 0, width, height, 0, 0, 0, 0);
 	HDC hdc = GetDC(hwnd);
 	PIXELFORMATDESCRIPTOR pfd = { 0,0,PFD_DOUBLEBUFFER };
-    SetPixelFormat(hdc,ChoosePixelFormat(hdc,&pfd),&pfd);
-    wglMakeCurrent(hdc,wglCreateContext(hdc));	
+	SetPixelFormat(hdc,ChoosePixelFormat(hdc,&pfd),&pfd);
+	wglMakeCurrent(hdc,wglCreateContext(hdc));	
 	((PFNWGLSWAPINTERVALEXTPROC)wglGetProcAddress("wglSwapIntervalEXT")) (0);
 	((PFNGLGENVERTEXARRAYSPROC)wglGetProcAddress("glGenVertexArrays")) (1, &VertexArrayID);		
 	((PFNGLBINDVERTEXARRAYPROC)wglGetProcAddress("glBindVertexArray")) (VertexArrayID);	
@@ -138,7 +138,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		((PFNGLVERTEXATTRIBPOINTERPROC)wglGetProcAddress("glVertexAttribPointer"))(1,2, GL_FLOAT, GL_FALSE, 0,(void*)0 );
 		glDrawArrays(GL_TRIANGLES, 0, 2*3);
 		((PFNGLDISABLEVERTEXATTRIBARRAYPROC)wglGetProcAddress("glDisableVertexAttribArray"))(0);
-		((PFNGLDISABLEVERTEXATTRIBARRAYPROC)wglGetProcAddress("glDisableVertexAttribArray"))(1);		
+		((PFNGLDISABLEVERTEXATTRIBARRAYPROC)wglGetProcAddress("glDisableVertexAttribArray"))(1);
 		wglSwapLayerBuffers(hdc, WGL_SWAP_MAIN_PLANE);
 	} 
 	return 0;
